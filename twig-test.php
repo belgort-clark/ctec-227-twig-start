d172a3e75c1d3d03824b245cb2ea8c5a946cfe04
belgort-clark <belgort@clark.edu>
2020-06-01 19:12:59 -0700

    Minor updates

--- twig-test.php
+++ twig-test.php
@@ -3,9 +3,11 @@ require_once 'vendor/autoload.php';
 
 $loader = new \Twig\Loader\FilesystemLoader('templates');
 
-$twig = new \Twig\Environment($loader, [
-    'cache' => '/path/to/compilation_cache',
-]);
+// $twig = new \Twig\Environment($loader, [
+//     'cache' => '/path/to/compilation_cache',
+// ]);
+
+$twig = new \Twig\Environment($loader);
 
 $links = [
     ['url' => 'http://bruceelgort.com', 'title' => 'The website of Bruce Elgort', 'text' => 'Bruce Elgort'],
